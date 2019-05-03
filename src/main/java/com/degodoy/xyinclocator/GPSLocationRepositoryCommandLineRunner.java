5c5f55547f41bb3a4d88ecbadf6f6cbbd801126e
octaviodegodoy <octaviodegodoy@gmail.com>
2019-05-03 01:22:05 -0300

    Implementing JPA for H2 db

--- src/main/java/com/degodoy/xyinclocator/GPSLocationRepositoryCommandLineRunner.java
+++ src/main/java/com/degodoy/xyinclocator/GPSLocationRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.degodoy.xyinclocator;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +21,13 @@ public class GPSLocationRepositoryCommandLineRunner implements CommandLineRunner
 	public void run(String... args) throws Exception {
 
 		GPSLocation gpsLocation = new GPSLocation(27, 38);
-		gpsLocationRepository.save(gpsLocation);
-		
+		gpsLocationRepository.save(gpsLocation);		
 		log.info("New GPS location is created : " + gpsLocation);
 		
+		Optional<GPSLocation> gpsOne = gpsLocationRepository.findById(1L);
+		
+		List<GPSLocation> gpsLocations = gpsLocationRepository.findAll();
+		log.info("All GPS locations : " + gpsLocations);
 		
 	}
 	
